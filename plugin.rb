ef3850435916f03b36238357ecd6016c1dce6c7a
Evg <budo@narod.ru>
2018-10-18 11:23:19 +0300

    Create plugin.rb

---
+++ plugin.rb
@@ -0,0 +1,13 @@
+# name: discourse-toxu-theme
+# about: design site Toxu.ru
+# version: 0.1
+# authors:  Evg
+# url: https://github.com/Toxuru/discourse-toxu-theme
+
+register_asset "stylesheets/style.scss"
+
+# Post extension
+  add_to_class :post, :excerpt_for_topic do
+      Post.excerpt(cooked, 300, strip_links: true)
+  end
+  add_to_serializer(:listable_topic, :include_excerpt?) { true }
