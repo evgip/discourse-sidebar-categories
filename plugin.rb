fae702a146c0dfe7d1642cbeaceeb5a8bbec2638
Evg <budo@narod.ru>
2018-10-18 11:44:47 +0300

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,12 @@
 
 register_asset "stylesheets/style.scss"
 
+after_initialize do
+  
 # Post extension
   add_to_class :post, :excerpt_for_topic do
       Post.excerpt(cooked, 300, strip_links: true)
   end
   add_to_serializer(:listable_topic, :include_excerpt?) { true }
+
+end
